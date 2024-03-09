7f71121fcd5cf28ab1b65831b389db40a5c20053
leatherbullet <al.obnizov@yandex.ru>
2024-03-09 09:50:22 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int size = 30;
+            int minValue = 0;
+            int maxValue = 10;
+
+            int[] numbers = new int[size];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(minValue,maxValue);
+                
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.SetCursorPosition(0,1);
+            Console.WriteLine("локальные максимумы");
+
+            for (int i = 1; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] > numbers[i + 1] && numbers[i] > numbers[i - 1])
+                {
+                    Console.WriteLine(numbers[i] + " ");
+                }
+            }
+        }
+    }
+}
